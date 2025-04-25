44ba050af7ecfaa82513ab822a686328a8bc76c6
Aljames Francis Pontigon <pontigon.115923@cubao.sti.edu.ph>
2025-04-25 13:20:36 +0800

    changes

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,34 +1,34 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./../globals.css"; // Ensures styles are applied globally
+// import { Geist, Geist_Mono } from "next/font/google";
+// import "./../globals.css"; // Ensures styles are applied globally
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
-export const metadata = {
-  title: "I-Hub",
-  description: "Inspire Hub",
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
-  },
-};
+// export const metadata = {
+//   title: "I-Hub",
+//   description: "Inspire Hub",
+//   icons: {
+//     icon: "/favicon.ico",
+//     shortcut: "/favicon.ico",
+//     apple: "/apple-touch-icon.png",
+//   },
+// };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
-      >
-        {children}
-      </body>
-    </html>
-  );
-}
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
