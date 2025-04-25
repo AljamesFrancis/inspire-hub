2e64948b4566909931bb2d7041d324224d2c942c
Aljames Francis Pontigon <pontigon.115923@cubao.sti.edu.ph>
2025-04-25 16:50:17 +0800

    marquee

--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import PartnerCompanies from "./components/PartnerCompanies";
 
 export default function Home() {
   return (
     
     <div>
       <Hero />
+      <PartnerCompanies />
     </div>
   );
 }
