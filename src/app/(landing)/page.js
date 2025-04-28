77b8fd3f8401a75edd01c517d5d537a45131540f
denseeen <villanueva.194566@cubao.sti.edu.ph>
2025-04-28 15:48:59 +0800

    landing

--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import Register from "./components/Register";
 
 export default function Home() {
   return (
     
     <div>
       <Hero />
+      <Register/>
     </div>
   );
 }
